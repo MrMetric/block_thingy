27fc67207083f5490dfcf0a240c88d93b3399985
Andrew Harrison <gamefreak1974@gmail.com>
2016-02-05 17:40:43 -0800

    Util::read_file: output file name on failure

--- src/Util.cpp
+++ src/Util.cpp
@@ -3,18 +3,27 @@
 #include <glad/glad.h>
 
 #include <fstream>
+#include <iostream>
+#include <memory>
 
 std::string Util::read_file(const std::string& path)
 {
-	std::ifstream inpoot(path, std::ios::ate | std::ios::binary);
-	inpoot.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-	uint_fast64_t fsize = static_cast<uint_fast64_t>(inpoot.tellg());
-	inpoot.seekg(0, std::ios::beg);
-	char* aaa = new char[fsize];
-	inpoot.read(aaa, fsize);
-	std::string bbb(aaa, fsize);
-	delete[] aaa;
-	return bbb;
+	try
+	{
+		std::ifstream inpoot(path, std::ios::ate | std::ios::binary);
+		inpoot.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		uint_fast64_t fsize = static_cast<uint_fast64_t>(inpoot.tellg());
+		inpoot.seekg(0, std::ios::beg);
+		std::unique_ptr<char[]> aaa = std::make_unique<char[]>(fsize);
+		inpoot.read(aaa.get(), static_cast<std::streamsize>(fsize));
+		std::string bbb(aaa.get(), fsize);
+		return bbb;
+	}
+	catch(const std::ios_base::failure&)
+	{
+		std::cerr << "failed to read " << path << "\n";
+		throw;
+	}
 }
 
 std::string Util::gl_error_string(const uint_fast32_t code)
