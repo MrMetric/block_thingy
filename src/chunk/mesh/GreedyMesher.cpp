3ea535b056ed389244061f8ac5c0cf81d9e39e4f
Andrew Harrison <gamefreak1974@gmail.com>
2016-03-01 07:11:23 -0800

    reorganized chunk/mesh/GreedyMesher

--- src/chunk/mesh/GreedyMesher.cpp
+++ src/chunk/mesh/GreedyMesher.cpp
@@ -42,10 +42,122 @@ GreedyMesher::GreedyMesher(const Chunk& chunk)
 	}
 }
 
-static void add_face(const mesh_vertex_coord_t& p1, const mesh_vertex_coord_t& p2, const mesh_vertex_coord_t& p3, const mesh_vertex_coord_t& p4, mesh_t& vertexes)
+meshmap_t GreedyMesher::make_mesh()
 {
-	vertexes.emplace_back(p1, p2, p3);
-	vertexes.emplace_back(p3, p4, p1);
+	meshmap_t meshes;
+
+	add_surface(meshes, Plane::XY, Side::top);
+	add_surface(meshes, Plane::XY, Side::bottom);
+	add_surface(meshes, Plane::XZ, Side::top);
+	add_surface(meshes, Plane::XZ, Side::bottom);
+	add_surface(meshes, Plane::YZ, Side::top);
+	add_surface(meshes, Plane::YZ, Side::bottom);
+
+	return meshes;
+}
+
+void GreedyMesher::add_surface(meshmap_t& meshes, Plane plane, Side side)
+{
+	uint_fast8_t ix, iy, iz;
+	if(plane == Plane::XY)
+	{
+		ix = 0;
+		iy = 2;
+		iz = 1;
+	}
+	else if(plane == Plane::XZ)
+	{
+		ix = 0;
+		iy = 1;
+		iz = 2;
+	}
+	else // must be YZ
+	{
+		ix = 1;
+		iy = 0;
+		iz = 2;
+	}
+
+	glm::tvec3<uint_fast8_t> xyz;
+	for(xyz[1] = 0; xyz[1] < CHUNK_SIZE; ++xyz[1])
+	{
+		generate_surface(xyz, ix, iy, iz, static_cast<int_fast8_t>(side));
+
+		while(true)
+		{
+			Rectangle rekt = yield_rectangle();
+			if(rekt.type == BlockType::none)
+			{
+				break;
+			}
+
+			mesh_vertex_coord_t v1;
+			mesh_vertex_coord_t v2;
+			mesh_vertex_coord_t v3;
+			mesh_vertex_coord_t v4;
+
+			uint8_t y1 = xyz[1];
+			if(side == Side::top)
+			{
+				y1 += 1;
+			}
+
+			#define s(x) static_cast<GLubyte>(x)
+			v1[ix] = rekt.x;
+			v1[iy] = y1;
+			v1[iz] = rekt.z;
+
+			v2[ix] = s(rekt.x + rekt.w);
+			v2[iy] = y1;
+			v2[iz] = rekt.z;
+
+			v3[ix] = s(rekt.x + rekt.w);
+			v3[iy] = y1;
+			v3[iz] = s(rekt.z + rekt.h);
+
+			v4[ix] = rekt.x;
+			v4[iy] = y1;
+			v4[iz] = s(rekt.z + rekt.h);
+			#undef s
+
+			mesh_t& vertexes = meshes[rekt.type];
+			if((plane == Plane::XZ && side == Side::top)
+			|| (plane == Plane::XY && side == Side::bottom)
+			|| (plane == Plane::YZ && side == Side::bottom))
+			{
+				add_face(v4, v3, v2, v1, vertexes);
+			}
+			else
+			{
+				add_face(v1, v2, v3, v4, vertexes);
+			}
+		}
+	}
+}
+
+void GreedyMesher::generate_surface(glm::tvec3<uint_fast8_t>& xyz, const uint_fast8_t ix, const uint_fast8_t iy, const uint_fast8_t iz, const int_fast8_t offset)
+{
+	for(xyz[0] = 0; xyz[0] < CHUNK_SIZE; ++xyz[0])
+	{
+		for(xyz[2] = 0; xyz[2] < CHUNK_SIZE; ++xyz[2])
+		{
+			uint_fast8_t x = xyz[ix];
+			uint_fast8_t y = xyz[iy];
+			uint_fast8_t z = xyz[iz];
+			int_fast8_t o[] = {0, 0, 0};
+			o[iy] = offset;
+
+			bool empty = block_is_invisible_not_none(x + o[0], y + o[1], z + o[2]);
+			if(empty && !block_is_invisible(x, y, z))
+			{
+				surface[xyz[2]][xyz[0]] = chunk.get_block_const({x, y, z}).type();
+			}
+			else
+			{
+				surface[xyz[2]][xyz[0]] = BlockType::none;
+			}
+		}
+	}
 }
 
 Rectangle GreedyMesher::yield_rectangle()
@@ -118,116 +230,8 @@ Rectangle GreedyMesher::yield_rectangle()
 	return { BlockType::none, 0, 0, 0, 0 };
 }
 
-void GreedyMesher::add_surface(meshmap_t& meshes, Plane plane, Side side)
+void GreedyMesher::add_face(const mesh_vertex_coord_t& p1, const mesh_vertex_coord_t& p2, const mesh_vertex_coord_t& p3, const mesh_vertex_coord_t& p4, mesh_t& vertexes)
 {
-	uint_fast8_t ix, iy, iz;
-	if(plane == Plane::XY)
-	{
-		ix = 0;
-		iy = 2;
-		iz = 1;
-	}
-	else if(plane == Plane::XZ)
-	{
-		ix = 0;
-		iy = 1;
-		iz = 2;
-	}
-	else // must be YZ
-	{
-		ix = 1;
-		iy = 0;
-		iz = 2;
-	}
-
-	glm::tvec3<uint_fast8_t> xyz;
-	for(xyz[1] = 0; xyz[1] < CHUNK_SIZE; ++xyz[1])
-	{
-		for(xyz[0] = 0; xyz[0] < CHUNK_SIZE; ++xyz[0])
-		{
-			for(xyz[2] = 0; xyz[2] < CHUNK_SIZE; ++xyz[2])
-			{
-				uint_fast8_t x = xyz[ix];
-				uint_fast8_t y = xyz[iy];
-				uint_fast8_t z = xyz[iz];
-				int_fast8_t offset = static_cast<int_fast8_t>(side);
-				int_fast8_t o[] = {0, 0, 0};
-				o[iy] = offset;
-
-				bool empty = block_is_invisible_not_none(x + o[0], y + o[1], z + o[2]);
-				if(empty && !block_is_invisible(x, y, z))
-				{
-					surface[xyz[2]][xyz[0]] = chunk.get_block_const({x, y, z}).type();
-				}
-				else
-				{
-					surface[xyz[2]][xyz[0]] = BlockType::none;
-				}
-			}
-		}
-
-		while(true)
-		{
-			Rectangle rekt = yield_rectangle();
-			if(rekt.type == BlockType::none)
-			{
-				break;
-			}
-
-			mesh_vertex_coord_t v1;
-			mesh_vertex_coord_t v2;
-			mesh_vertex_coord_t v3;
-			mesh_vertex_coord_t v4;
-
-			uint8_t y1 = xyz[1];
-			if(side == Side::top)
-			{
-				y1 += 1;
-			}
-
-			#define s(x) static_cast<GLubyte>(x)
-			v1[ix] = rekt.x;
-			v1[iy] = y1;
-			v1[iz] = rekt.z;
-
-			v2[ix] = s(rekt.x + rekt.w);
-			v2[iy] = y1;
-			v2[iz] = rekt.z;
-
-			v3[ix] = s(rekt.x + rekt.w);
-			v3[iy] = y1;
-			v3[iz] = s(rekt.z + rekt.h);
-
-			v4[ix] = rekt.x;
-			v4[iy] = y1;
-			v4[iz] = s(rekt.z + rekt.h);
-			#undef s
-
-			mesh_t& vertexes = meshes[rekt.type];
-			if((plane == Plane::XZ && side == Side::top)
-			|| (plane == Plane::XY && side == Side::bottom)
-			|| (plane == Plane::YZ && side == Side::bottom))
-			{
-				add_face(v4, v3, v2, v1, vertexes);
-			}
-			else
-			{
-				add_face(v1, v2, v3, v4, vertexes);
-			}
-		}
-	}
-}
-
-meshmap_t GreedyMesher::make_mesh()
-{
-	meshmap_t meshes;
-
-	add_surface(meshes, Plane::XY, Side::top);
-	add_surface(meshes, Plane::XY, Side::bottom);
-	add_surface(meshes, Plane::XZ, Side::top);
-	add_surface(meshes, Plane::XZ, Side::bottom);
-	add_surface(meshes, Plane::YZ, Side::top);
-	add_surface(meshes, Plane::YZ, Side::bottom);
-
-	return meshes;
+	vertexes.emplace_back(p1, p2, p3);
+	vertexes.emplace_back(p3, p4, p1);
 }
--- src/chunk/mesh/GreedyMesher.hpp
+++ src/chunk/mesh/GreedyMesher.hpp
@@ -22,6 +22,8 @@ class GreedyMesher : public ChunkMesher
 	private:
 		std::vector<std::vector<BlockType>> surface;
 
-		GreedyMesherPrivate::Rectangle yield_rectangle();
 		void add_surface(meshmap_t&, const GreedyMesherPrivate::Plane, const GreedyMesherPrivate::Side);
+		void generate_surface(glm::tvec3<uint_fast8_t>&, const uint_fast8_t, const uint_fast8_t, const uint_fast8_t, const int_fast8_t);
+		GreedyMesherPrivate::Rectangle yield_rectangle();
+		void add_face(const mesh_vertex_coord_t& p1, const mesh_vertex_coord_t& p2, const mesh_vertex_coord_t& p3, const mesh_vertex_coord_t& p4, mesh_t& vertexes);
 };
