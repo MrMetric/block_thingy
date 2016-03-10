121293c2b8a1251d017d17161fe33b2ca49489e4
Andrew Harrison <gamefreak1974@gmail.com>
2016-03-09 21:39:03 -0800

    improved efficiency [speed] of 'dots' block shader

--- bin/shaders/block/dots.fs
+++ bin/shaders/block/dots.fs
@@ -9,6 +9,33 @@ float discretestep(float split, float x)
 	return (sign(x - split) + 1.0) / 2.0;
 }
 
+int get_side()
+{
+	vec3 f = fract(vPos);
+	if(f.x != 0.0 && f.y != 0.0 && f.z == 0.0)
+	{
+		return 0;
+	}
+	if(f.x != 0.0 && f.y == 0.0 && f.z != 0.0)
+	{
+		return 1;
+	}
+	if(f.x == 0.0 && f.y != 0.0 && f.z != 0.0)
+	{
+		return 2;
+	}
+	return -1;
+}
+
+vec2 get_side_coords()
+{
+	int side = get_side();
+	if(side == 0) return vPos.xy;
+	if(side == 1) return vPos.xz;
+	if(side == 2) return vPos.yz;
+	return vec2(0.0);
+}
+
 float radius = 0.3 * (sin(4.0 * global_time + length(vPos))/4.0 + 1.0);
 const float radius_max = 0.3 * (1 / 4.0 + 1.0);
 float radius_scaled = radius / radius_max;
@@ -28,9 +55,8 @@ float color(vec2 vec)
 
 void main()
 {
-	float c = color(vPos.xy) + color(vPos.xz) + color(vPos.yz);
-	vec3 vColor = vec3(c, c, c);
-	vColor *= hsv2rgb(vec3(global_time + radius_scaled, 1.0, 1.0));
-	vColor *= radius_scaled;
-	FragColor = vec4(vColor, 1.0);
+	vec3 c = vec3(color(get_side_coords()));
+	c *= hsv2rgb(vec3(global_time + radius_scaled, 1.0, 1.0));
+	c *= radius_scaled;
+	FragColor = vec4(c, 1.0);
 }
