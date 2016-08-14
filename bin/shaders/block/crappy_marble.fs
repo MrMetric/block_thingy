a804df175d36d3cc218476b18a12baa616ae1b7b
Andrew Harrison <gamefreak1974@gmail.com>
2016-08-14 11:06:59 -0700

    crappy_marble and light_test shaders: fix undefined calculation (pow of negative)

--- bin/shaders/block/crappy_marble.fs
+++ bin/shaders/block/crappy_marble.fs
@@ -22,8 +22,8 @@ const float amplitude = 1.0;
 vec3 color(vec2 coords)
 {
 	vec2 n = coords + amplitude * turbulence(coords);
-	float c1 = pow(cos(M_TAU / 2.0 * n.x), 1.0/6.0);
-	float c2 = pow(cos(M_TAU / 2.0 * n.y), 1.0/4.0);
+	float c1 = pow(abs(cos(M_TAU / 2.0 * n.x)), 1.0 / 6.0);
+	float c2 = pow(abs(cos(M_TAU / 2.0 * n.y)), 1.0 / 4.0);
 	vec3 c = vec3((c1 + c2) / 2.0);
 	c.r *= 0.975;
 	c.b *= 0.975;
--- bin/shaders/block/light_test.fs
+++ bin/shaders/block/light_test.fs
@@ -23,8 +23,8 @@ vec3 color(vec2 coords)
 	}
 	*/
 
-	float cx = pow(sin(M_TAU / 2.0 * coords.x), 1.0 / 8.0);
-	float cy = pow(sin(M_TAU / 2.0 * coords.y), 1.0 / 8.0);
+	float cx = pow(abs(sin(M_TAU / 2.0 * coords.x)), 1.0 / 8.0);
+	float cy = pow(abs(sin(M_TAU / 2.0 * coords.y)), 1.0 / 8.0);
 	return vec3(1.0 - cx * cy);
 }
 
