c575e96b2e3833e8c9ab76980b7411e1b46d5b1d
Andrew Harrison <gamefreak1974@gmail.com>
2016-08-29 23:35:26 -0700

    improve GUI button appearance

--- src/graphics/GUI/Widget/Button.cpp
+++ src/graphics/GUI/Widget/Button.cpp
@@ -17,7 +17,7 @@ Button::Button
 )
 :
 	Base(owner, {256, 64}),
-	color(0, 0, 0, 0.8),
+	color(0.01, 0.01, 0.02, 0.85),
 	hover_color(0, 0, 0, 1),
 	hover(false),
 	mousedown(false),
@@ -30,7 +30,22 @@ Button::Button
 
 void Button::draw()
 {
+	const double border_size = 2.0;
+	const glm::dvec4 border_color(1.0);
+	const glm::dvec2 border_size_x(size.x + border_size * 2, border_size);
+	const glm::dvec2 border_size_y(border_size, size.y + border_size * 2);
+
+	const glm::dvec2 border_pos_topleft(real_position - border_size);
+	const glm::dvec2 border_pos_bottom(real_position.x - border_size, real_position.y + size.y);
+	const glm::dvec2 border_pos_right(real_position.x + size.x, real_position.y - border_size);
+
+	owner.game.gfx.draw_rectangle(border_pos_topleft, border_size_x, border_color);
+	owner.game.gfx.draw_rectangle(border_pos_bottom, border_size_x, border_color);
+	owner.game.gfx.draw_rectangle(border_pos_topleft, border_size_y, border_color);
+	owner.game.gfx.draw_rectangle(border_pos_right, border_size_y, border_color);
+
 	owner.game.gfx.draw_rectangle(real_position, size, hover ? hover_color : color);
+
 	owner.game.gfx.gui_text.draw(text, glm::round(real_position + text_position));
 }
 
