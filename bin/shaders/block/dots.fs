2cb4b71e4d2399e3341855c336528e0b08c473b0
Mr. Metric <gamefreak1974@gmail.com>
2016-12-18 21:14:46 -0800

    improve 'dots' block shader (no visual change)

--- bin/shaders/block/dots.fs
+++ bin/shaders/block/dots.fs
@@ -1,8 +1,6 @@
 #include include/header.fs
 
-float radius = 0.3 * (sin(4.0 * global_time + length(position))/4.0 + 1.0);
-const float radius_max = 0.3 * (1.0 / 4.0 + 1.0);
-float radius_scaled = radius / radius_max;
+const float radius_max = 1.0 / 4.0 + 1.0;
 
 // http://lolengine.net/blog/2013/07/27/rgb-to-hsv-in-glsl
 vec3 hsv2rgb(vec3 c)
@@ -14,7 +12,9 @@ vec3 hsv2rgb(vec3 c)
 
 vec4 color(vec2 coords)
 {
-	vec3 c = vec3(1.0 - discretestep(radius, length(fract(coords) - 0.5)));
+	float radius = sin(4.0 * global_time + length(position)) / 4.0 + 1.0;
+	float radius_scaled = radius / radius_max;
+	vec3 c = vec3(1.0 - discretestep(0.3 * radius, length(fract(coords) - 0.5)));
 	c *= hsv2rgb(vec3(global_time + radius_scaled, 1.0, 1.0));
 	c *= radius_scaled;
 	return vec4(c, 1.0);
