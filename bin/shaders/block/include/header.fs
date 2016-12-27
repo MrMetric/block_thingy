3ba6dee341371a679b4ebd0c19e76f65aa5bf963
Mr. Metric <gamefreak1974@gmail.com>
2016-12-26 23:53:45 -0800

    block shaders: fix get_side's precision issue (it worked on my Intel card, but not my NVIDIA card)

--- bin/shaders/block/include/header.fs
+++ bin/shaders/block/include/header.fs
@@ -8,24 +8,40 @@ uniform float global_time;
 
 float discretestep(float split, float x)
 {
-	return (sign(x - split) + 1.0) / 2.0;
+	return (sign(x - split) + 1) / 2;
 }
 
 int get_side()
 {
 	vec3 f = fract(position);
-	if(f.x != 0.0 && f.y != 0.0 && f.z == 0.0)
+
+	bool zx = f.z < f.x;
+	bool z1x = f.z < 1 - f.x;
+	bool zy = f.z < f.y;
+	bool z1y = f.z < 1 - f.y;
+	if(zx == z1x && zy == z1y)
 	{
 		return 0;
 	}
-	if(f.x != 0.0 && f.y == 0.0 && f.z != 0.0)
+
+	bool yx = f.y < f.x;
+	bool y1x = f.y < 1 - f.x;
+	bool yz = !zy;
+	bool y1z = z1y;
+	if(yx == y1x && yz == y1z)
 	{
 		return 1;
 	}
-	if(f.x == 0.0 && f.y != 0.0 && f.z != 0.0)
+
+	bool xz = !zx;
+	bool x1z = z1x;
+	bool xy = !yx;
+	bool x1y = y1x;
+	if(xz == x1z && xy == x1y)
 	{
 		return 2;
 	}
+
 	return -1;
 }
 
@@ -35,5 +51,5 @@ vec2 get_side_coords()
 	if(side == 0) return position.xy;
 	if(side == 1) return position.xz;
 	if(side == 2) return position.zy;
-	return vec2(0.0);
+	return vec2(0);
 }
