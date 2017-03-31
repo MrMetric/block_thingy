MrMetric/block_thingy

commit 8f43c63d16a3317b055c4f58c35d7314be946571

misc improvements

src/Game.cpp
@@ -1,6 +1,13 @@
 #include "Game.hpp"
 
 #include <cmath>
+#if __has_include(<filesystem>)
+	#include <filesystem>
+	namespace fs = std::filesystem;
+#else
+	#include <experimental/filesystem>
+	namespace fs = std::experimental::filesystem;
+#endif
 #include <fstream>
 #include <functional>
 #include <limits>
@@ -282,7 +289,7 @@ void Game::quit()
 
 void Game::screenshot(string filename)
 {
-	if(Util::create_directory("screenshots"))
+	if(fs::create_directory("screenshots"))
 	{
 		filename = "screenshots/" + filename;
 	}

CMakeLists.txt
@@ -35,7 +35,7 @@ pkg_search_module(FREETYPE REQUIRED freetype2)
 pkg_search_module(LIBPNG REQUIRED libpng)
 pkg_search_module(MSGPACK REQUIRED msgpack>=2.0.0) # TODO: determine minimum version
 
-if(${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
+if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
 	set(FSANITIZE -fsanitize=undefined,integer)
 endif()
 

block_thingy.cbp
@@ -78,6 +78,7 @@
 			<Add option="`pkg-config --cflags msgpack`" />
 			<Add option="-Weverything" />
 			<Add option="-Wno-c++98-compat-pedantic" />
+			<Add option="-Wno-documentation-unknown-command" />
 			<Add option="-Wno-exit-time-destructors" />
 			<Add option="-Wno-global-constructors" />
 			<Add option="-Wno-padded" />
@@ -104,6 +105,8 @@
 			<Add option="-ldl" />
 			<Add option="-lz" />
 		</Linker>
+		<Unit filename="lib/easylogging++/easylogging++.cpp" />
+		<Unit filename="lib/easylogging++/easylogging++.hpp" />
 		<Unit filename="lib/inotify-cxx/inotify-cxx.cpp" />
 		<Unit filename="lib/inotify-cxx/inotify-cxx.hpp" />
 		<Unit filename="src/Camera.cpp" />

lib/easylogging++/easylogging++.hpp
@@ -802,12 +802,12 @@ const struct {
   double value;
   const base::type::char_t* unit;
 } kTimeFormats[] = {
-  { 1000.0f, ELPP_LITERAL("us") },
-  { 1000.0f, ELPP_LITERAL("ms") },
-  { 60.0f, ELPP_LITERAL("seconds") },
-  { 60.0f, ELPP_LITERAL("minutes") },
-  { 24.0f, ELPP_LITERAL("hours") },
-  { 7.0f, ELPP_LITERAL("days") }
+  { 1000.0, ELPP_LITERAL("us") },
+  { 1000.0, ELPP_LITERAL("ms") },
+  { 60.0, ELPP_LITERAL("seconds") },
+  { 60.0, ELPP_LITERAL("minutes") },
+  { 24.0, ELPP_LITERAL("hours") },
+  { 7.0, ELPP_LITERAL("days") }
 };
 static const int kTimeFormatsCount                           =      sizeof(kTimeFormats) / sizeof(kTimeFormats[0]);
 const struct {

src/Game.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <experimental/propagate_const>
 #include <memory>
+#include <experimental/propagate_const>
 #include <string>
 
 #include <glm/mat4x4.hpp>

src/Gfx.cpp
@@ -133,6 +133,10 @@ GLFWwindow* Gfx::init_glfw()
 	});
 	glfwSetKeyCallback(window, [](GLFWwindow*, int key, int scancode, int action, int mods)
 	{
+		if(key == GLFW_KEY_UNKNOWN)
+		{
+			return;
+		}
 		Game::instance->keypress(key, scancode, action, Util::key_mods(mods));
 	});
 	glfwSetCharModsCallback(window, [](GLFWwindow*, unsigned int codepoint, int mods)

src/ResourceManager.cpp
@@ -12,11 +12,11 @@
 #include "graphics/OpenGL/ShaderObject.hpp"
 
 #if __has_include(<filesystem>)
-#include <filesystem>
-namespace fs = std::filesystem;
+	#include <filesystem>
+	namespace fs = std::filesystem;
 #else
-#include <experimental/filesystem>
-namespace fs = std::experimental::filesystem;
+	#include <experimental/filesystem>
+	namespace fs = std::experimental::filesystem;
 #endif
 
 using std::cerr;

src/Util.cpp
@@ -203,35 +203,6 @@ void Util::change_directory(const string& path)
 	#endif
 }
 
-// http://en.cppreference.com/w/cpp/experimental/fs/create_directory
-bool Util::create_directory(const string& path)
-{
-	#ifdef _WIN32
-	if(PathIsDirectory(path.c_str()))
-	#else
-	struct stat s;
-	if(stat(path.c_str(), &s) == 0 && s.st_mode & S_IFDIR)
-	#endif
-	{
-		return true;
-	}
-
-	return mkdir(path.c_str(), 0755) == 0;
-}
-
-bool Util::create_directories(const string& path)
-{
-	if(create_directory(path))
-	{
-		return true;
-	}
-	if(errno == ENOENT && create_directories(path.substr(0, path.find_last_of('/'))))
-	{
-		return create_directory(path);
-	}
-	return false;
-}
-
 int Util::stoi(const string& s)
 {
 	if(s.find_first_not_of("0123456789") != string::npos)

commit a814d1698a34770c733901a9ff923c052d24d42f

fix using screenshots dir for screenshots
This error was introduced in 8f43c63d16a3317b055c4f58c35d7314be946571. My create_directory returned true (success) when the directory already exists, whereas C++'s create_directory returns false.

src/Game.cpp
@@ -292,7 +292,7 @@ void Game::quit()
 
 void Game::screenshot(string filename) const
 {
-	if(fs::create_directory("screenshots"))
+	if(fs::is_directory("screenshots") || fs::create_directory("screenshots"))
 	{
 		filename = "screenshots/" + filename;
 	}