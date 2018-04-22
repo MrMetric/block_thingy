f9d3cf7820d3d6074d13bb5298dfa7616f5e8dd4
Mr. Metric <thingies.email@gmail.com>
2018-04-21 21:50:07 -0700

    add ICC version to util::compiler_info + format at compile-time, not run-time

--- src/util/compiler_info.cpp
+++ src/util/compiler_info.cpp
@@ -1,33 +1,70 @@
 #include "compiler_info.hpp"
 
-#ifdef _MSC_VER
-	#include <iomanip>
-	#include <sstream>
-#endif
-
 using std::string;
 
 namespace block_thingy::util {
+
+#if !defined(__ICC) && defined(__ICL)
+	// TODO: test this
+	#define __ICC __ICL
+#endif
+
+#if defined(__ICC)
+static const char info[]
+{
+	// C2 AE is UTF-8 for ®
+	'I', 'n', 't', 'e', 'l', 0xC2, 0xAE, ' ',
+	'C', '+', '+', ' ',
+	'C', 'o', 'm', 'p', 'i', 'l', 'e', 'r', ' ',
+	(__ICC / 1000) % 10 + '0',
+	(__ICC /  100) % 10 + '0',
+	'.',
+	(__ICC /   10) % 10 + '0',
+	'.',
+	(__ICC /    1) % 10 + '0',
+	' ',
+	(__INTEL_COMPILER_BUILD_DATE / 10000000) % 10 + '0',
+	(__INTEL_COMPILER_BUILD_DATE /  1000000) % 10 + '0',
+	(__INTEL_COMPILER_BUILD_DATE /   100000) % 10 + '0',
+	(__INTEL_COMPILER_BUILD_DATE /    10000) % 10 + '0',
+	(__INTEL_COMPILER_BUILD_DATE /     1000) % 10 + '0',
+	(__INTEL_COMPILER_BUILD_DATE /      100) % 10 + '0',
+	(__INTEL_COMPILER_BUILD_DATE /       10) % 10 + '0',
+	(__INTEL_COMPILER_BUILD_DATE /        1) % 10 + '0',
+};
+#elif defined(_MSC_VER)
+static const char info[]
+{
+	'M', 'S', 'V', 'C', ' ',
+	(_MSC_FULL_VER / 100000000) % 10 + '0',
+	(_MSC_FULL_VER /  10000000) % 10 + '0',
+	'.',
+	(_MSC_FULL_VER /   1000000) % 10 + '0',
+	(_MSC_FULL_VER /    100000) % 10 + '0',
+	'.',
+	(_MSC_FULL_VER /     10000) % 10 + '0',
+	(_MSC_FULL_VER /      1000) % 10 + '0',
+	(_MSC_FULL_VER /       100) % 10 + '0',
+	(_MSC_FULL_VER /        10) % 10 + '0',
+	(_MSC_FULL_VER /         1) % 10 + '0',
+	'.',
+	(_MSC_BUILD / 10) % 10 + '0',
+	(_MSC_BUILD /  1) % 10 + '0',
+};
+#endif
 
 string compiler_info()
 {
 #if defined(__clang__)
 	return "Clang " __clang_version__;
 #elif defined(__INTEL_COMPILER)
-	// TODO: version
-	return "Intel Compiler";
+	return info;
 #elif defined(__GNUC__)
 	return "GCC " __VERSION__;
 #elif defined(_MSC_VER)
-	std::ostringstream s;
-	s << std::setfill('0');
-	s << std::setw(2) << _MSC_VER / 100 << '.';
-	s << std::setw(2) << _MSC_VER % 100 << '.';
-	s << std::setw(5) << _MSC_FULL_VER % 100000 << '.';
-	s << std::setw(2) << _MSC_BUILD;
-	return "MSVC " + s.str();
+	return info;
 #else
-	return "";
+	return "(unknown compiler)";
 #endif
 }
 
